76d0046bde339889bbb6b85ccc98efdd6f581d9e
Connor Brown <falconnor4@gmail.com>
2026-04-28 21:50:52 -0700

    Add nix build support for flake

--- flake.nix
+++ flake.nix
@@ -7,12 +7,72 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    let
+      overlay = final: prev:
+        let
+          llvm = final.llvmPackages_18;
+          vulkan-loader = final.vulkan-loader;
+          vulkan-validation-layers = final.vulkan-validation-layers;
+        in
+        {
+          lin = final.stdenv.mkDerivation {
+            pname = "lin";
+            version = "0.1.0";
+
+            src = ./.;
+
+            nativeBuildInputs = [
+              llvm.llvm
+              llvm.bintools
+              llvm.clang
+              llvm.mlir
+              llvm.mlir.dev
+              final.cmake
+              final.ninja
+              final.python3
+              final.lit
+              final.gtest
+              final.spirv-tools
+              final.vulkan-headers
+              vulkan-loader
+              vulkan-validation-layers
+            ];
+
+            configurePhase = ''
+              export LLVM_DIR=${llvm.llvm.dev}
+              export MLIR_DIR=${llvm.mlir.dev}
+              export LD_LIBRARY_PATH="${vulkan-loader}/lib:${vulkan-validation-layers}/lib:$LD_LIBRARY_PATH"
+              export VK_LAYER_PATH="${vulkan-validation-layers}/share/vulkan/explicit_layer.d"
+
+              rm -rf build
+              cmake -G Ninja -B build \
+                -DLLVM_DIR=$out/lib/cmake/llvm \
+                -DMLIR_DIR=$out/lib/cmake/mlir \
+                -DCMAKE_BUILD_TYPE=Release \
+                .
+            '';
+
+            buildPhase = ''
+              ninja -C build
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/src/linc $out/bin/
+            '';
+          };
+        };
+    in
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
         llvm = pkgs.llvmPackages_18;
+        vulkan-loader = pkgs.vulkan-loader;
+        vulkan-validation-layers = pkgs.vulkan-validation-layers;
       in
       {
+        packages.default = pkgs.lin;
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             llvm.llvm
@@ -27,15 +87,15 @@
             pkgs.gtest
             pkgs.spirv-tools
             pkgs.vulkan-headers
-            pkgs.vulkan-loader
-            pkgs.vulkan-validation-layers
+            vulkan-loader
+            vulkan-validation-layers
           ];
 
           shellHook = ''
             export LLVM_DIR=${llvm.llvm.dev}
             export MLIR_DIR=${llvm.mlir.dev}
-            export LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib:${pkgs.vulkan-validation-layers}/lib:$LD_LIBRARY_PATH"
-            export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
+            export LD_LIBRARY_PATH="${vulkan-loader}/lib:${vulkan-validation-layers}/lib:$LD_LIBRARY_PATH"
+            export VK_LAYER_PATH="${vulkan-validation-layers}/share/vulkan/explicit_layer.d"
             echo "Lin Development Environment"
             echo "LLVM/MLIR Version: ${llvm.llvm.version}"
           '';
